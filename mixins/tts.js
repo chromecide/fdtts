2fdee1cbe1db4357fa73ee947cb50829921c275c
Justin Pradier <chromecide@chromecide.com>
2013-09-01 13:22:13 +1000

    added voice and rate parameters

--- mixins/tts.js
+++ mixins/tts.js
@@ -8,17 +8,40 @@ define(['require'], function(require){
 		//called when first mixing in the functionality
 		init: function(cfg, callback){
 			var self = this;
+
+			for(var key in cfg){
+				self.set(key, cfg[key]);
+			}
+
+			if(!self.get('voice')){
+				self.set('voice', 'Alex');
+			}
+
+			if(!self.get('rate')){
+				self.set('rate', 175);
+			}
+
 			if(callback){
-			    callback(false, cfg);
+				callback(false, cfg);
 			}
 		},
 		//called when something is published to this channel
 		publish: function(topic, data){
 			var self = this;
+			var voice = self.get('voice');
+			var rate = self.get('rate');
+
+			if(data.voice){
+				voice = data.voice;
+			}
+
+			if(data.rate){
+				rate = data.rate;
+			}
 
 			switch(topic){
 				case 'tts.say':
-					require('child_process').exec('say '+data.text);
+					require('child_process').exec('say -v '+voice+' -r '+rate+' '+data.text);
 					break;
 			}
 		}
